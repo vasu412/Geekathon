8c5d8c28737300d03e1468fa03e7e502e14d7669
Swaraj Anand <swrjnnd@gmail.com>
2024-02-04 14:15:17 +0530

    Completd{Result page} Created->Driver data

---
+++ Swaraj/DriversPage/Js/drivers.js
@@ -0,0 +1,209 @@
+const Drivers = [
+  {
+    name: "MAX",
+    club: "VERSTAPPEN",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/Netherlands.jpg.transform/2col/image.jpg",
+    car: "Red Bull Racing",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/MAXVER01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col/image.png",
+    border: "#3671C6",
+  },
+  {
+    name: "SERGIO",
+    club: "PEREZ",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/Mexico.jpg.transform/2col/image.jpg",
+    car: "Red Bull Racing",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/SERPER01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png.transform/2col/image.png",
+    border: "#3671C6",
+  },
+  {
+    name: "GEORGE",
+    club: "RUSELL",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/United%20Kingdom.jpg.transform/2col/image.jpg",
+    car: "Mercedes",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/GEORUS01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/2col/image.png",
+    border: "#6CD3BF",
+  },
+  {
+    name: "LEWIS",
+    club: "HAMILTON",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/United%20Kingdom.jpg.transform/2col/image.jpg",
+    car: "Mercedes",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/LEWHAM01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/2col/image.png",
+    border: "#6CD3BF",
+  },
+  {
+    name: "CHARLES",
+    club: "LECLERC",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/Monaco.jpg.transform/2col/image.jpg",
+    car: "Ferrari",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/CHALEC01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col/image.png",
+    border: "#F91536",
+  },
+  {
+    name: "CARLOS",
+    club: "SAINZ",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/Spain.jpg.transform/2col/image.jpg",
+    car: "Ferrari",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/CARSAI01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png.transform/2col/image.png",
+    border: "#F91536",
+  },
+  {
+    name: "OSCAR",
+    club: "PIASTRI",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/Australia.jpg.transform/2col/image.jpg",
+    car: "McLaren",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/OSCPIA01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png.transform/2col/image.png",
+    border: "#F58020",
+  },
+  {
+    name: "LANDO",
+    club: "NORIS",
+    flag: "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/flags/United%20Kingdom.jpg.transform/2col/image.jpg",
+    car: "McLaren",
+    number:
+      "https://media.formula1.com/d_default_fallback_image.png/content/dam/fom-website/2018-redesign-assets/drivers/number-logos/LANNOR01.png.transform/2col/image.png",
+    image:
+      "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/2col/image.png",
+    border: "#F58020",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+  {
+    name: "",
+    club: "",
+    flag: "",
+    car: "",
+    number: "",
+    image: "",
+    border: "",
+  },
+];
+
+export default Drivers;
